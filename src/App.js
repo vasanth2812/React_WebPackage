6f28ce6d3da1b3fc99bea558ad7bb052ee852676
vasanth kumar r <vasanth3422@gmail.com>
2019-07-27 23:03:23 +0530

    package setup with Reactstrap,Redux & ESLINT and Pretier

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Button } from "reactstrap";
 import { Route } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Header from "./Component/Header/Header";
 import HomePage from "./Modules/HomePage/HomePage";
 import LoginPage from "./Modules/LoginPage/LoginPage";
+import "./global/global.scss";
 
 const App = () => (
   <div>
+    <Header />
     <Route path="/" exact component={HomePage} />
     <Route path="/login" exact component={LoginPage} />
   </div>
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <div className="container">
+            <NavbarBrand tag={Link} to="/">
+              NanoPrecise
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink tag={Link} to="/login">
+                    Login
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} to="/signup">
+                    Signup
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
