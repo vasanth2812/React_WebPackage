f724eae9b8d15711cbcc5da6da7548dc8a48b41a
vasanth kumar r <vasanth3422@gmail.com>
2019-07-27 21:36:53 +0530

    Prettier setup

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./HomePage.scss";
 
-const HomePage = () =>(
-    <div className="container anchor">
-        <h5>HomePage</h5>
-        <Link to="/login">Login</Link>
-    </div>
+const HomePage = () => (
+  <div className="container anchor">
+    <h5>HomePage</h5>
+    <Link to="/login">Login</Link>
+  </div>
 );
 
 export default HomePage;
--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const LoginPage = () => (
-    <div className="container">
-        <h5>LoginPage</h5>
-    </div>
+  <div className="container">
+    <h5>LoginPage</h5>
+  </div>
 );
 
 export default LoginPage;
