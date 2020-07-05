0863b0bcd87b4c7dd2bfb433bfca2d4dcff2c6e1
vasanth kumar r <vasanth3422@gmail.com>
2020-07-05 18:19:31 +0530

    updated react package & installed react saga

--- src/Store/store.js
+++ src/Store/store.js
@@ -4,7 +4,9 @@ import { persistReducer } from "redux-persist";
 import LocalStorage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 import rootReducer from "./rootReducer";
+import { rootSaga } from "./rootSaga";
 
 const persistConfig = {
   key: "root",
@@ -12,10 +14,13 @@ const persistConfig = {
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   persistedReducer,
-  composeWithDevTools(applyMiddleware(thunk, logger))
+  composeWithDevTools(applyMiddleware(sagaMiddleware, thunk, logger))
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
