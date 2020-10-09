9170f4e9713e216743eb5e9e4867647df587e015
vasanth kumar r <vasanth3422@gmail.com>
2020-10-09 21:00:02 +0530

    handled 404 page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Button } from "reactstrap";
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Header from "./Component/Header/Header";
-import HomePage from "./Modules/HomePage/HomePage";
-import LoginPage from "./Modules/LoginPage/LoginPage";
+import HomePage from "./Modules/HomePage";
+import LoginPage from "./Modules/LoginPage";
+import NotFound from "./Modules/NotFound";
 import "./global/global.scss";
 
 const App = () => (
   <div>
     <Header />
-    <Route path="/" exact component={HomePage} />
-    <Route path="/login" exact component={LoginPage} />
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/login" exact component={LoginPage} />
+      <Route path="*" exact component={NotFound} />
+    </Switch>
   </div>
 );
 
